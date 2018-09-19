dbd6d9c9c63563d7f728f45669673fe7d22e1c6f
Todd Milliken <milliktr@gmail.com>
2018-09-19 16:45:21 -0400

    Enqueue gutenberg assets to admin scripts
    Enqueues gutenberg assets needed to use the `wp.blocks.registerBlockType()` method in JS. Eventually these won’t be needed for getting blocks since there will be a more stable (server-side) API in the future.

--- admin/class-bu-blocks-admin.php
+++ admin/class-bu-blocks-admin.php
@@ -23,42 +23,113 @@
 class BU_Blocks_Admin {
 
 	/**
-	 * Array of BU Block instances.
-	 *
-	 * See /includes/class-bu-blocks.php for when all block classes
-	 * were instantiated and passed into this class's __construct method.
-	 *
-	 * @since    1.0.0
-	 * @var      array $block_instances An array containing "BU_Block Class name" => BU_Block instance
-	 *                                  value pairs.
-	 */
-	public $block_instances = array();
-
-	/**
-	 * WP_List_Table object
+	 * The ID of this plugin.
 	 *
 	 * @since    1.0.0
 	 * @access   private
-	 * @var      block_list_table    $block_list_table
+	 * @var      string    $plugin_name    The ID of this plugin.
 	 */
-	private $block_list_table;
+	private $plugin_name;
+
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $version    The current version of this plugin.
+	 */
+	private $version;
 
 	/**
 	 * Initialize the class and set its properties.
 	 *
 	 * @since    1.0.0
+	 * @param    string $plugin_name The name of this plugin. Used for admin pages and script names.
+	 * @param    string $version     The version of this plugin. Used for cache-busting.
 	 */
-	public function __construct( $block_instances = array() ) {
-		$this->block_instances = $block_instances;
+	public function __construct( $plugin_name, $version ) {
+		$this->plugin_name = $plugin_name;
+		$this->version     = $version;
 	}
 
 	/**
 	 * Register the stylesheets for the admin area.
 	 *
 	 * @since    1.0.0
+	 *
+	 * @param    string $hook_suffix The name of the admin page.
 	 */
-	public function enqueue_styles() {
-		wp_enqueue_style( 'bu-blocks', BU_BLOCKS_DIR_URL . '/admin/admin.css', array(), null, 'all' );
+	public function enqueue_styles( $hook_suffix ) {
+		// Targets specific admin pages to enqueue bu-blocks styles.
+		if ( in_array( $hook_suffix, $this->get_admin_pages_for_scripts(), true ) ) {
+			wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/bu-blocks-admin.css', array(), $this->version, 'all' );
+		}
+	}
+
+	/**
+	 * Register the JavaScript for the admin area.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @param    string $hook_suffix The name of the admin page.
+	 */
+	public function enqueue_scripts( $hook_suffix ) {
+		/**
+		 * There are only 6 Server-side blocks, retrieved using
+		 * `WP_Block_Type_Registry::get_instance()->get_all_registered()`:
+		 *
+		 * - core/block (generic block model?)
+		 * - core/latest-comments
+		 * - core/archives
+		 * - core/categories
+		 * - core/latest-posts
+		 * - core/shortcode
+		 *
+		 * This might be because these blocks are "dynamic" blocks,
+		 * which are blocks that need to fetch content from the server at runtime.
+		 *
+		 * There are plans to either move all blocks to use server-side registration,
+		 * or expose all registered block data on the server-side in another way
+		 * where block data can reliably be retrieved.
+		 * https://github.com/WordPress/gutenberg/issues/8352)
+		 *
+		 * Until then, the gutenberg js assets must be enqueued so the plugin can
+		 * detect all registered blocks via the JavaScript API that gutenberg gives us,
+		 * using `wp.blocks.getBlockTypes()`.
+		 */
+
+		// Targets specific admin pages to enqueue bu-blocks scripts.
+		if ( in_array( $hook_suffix, $this->get_admin_pages_for_scripts(), true ) ) {
+			/**
+			 * Defined in `wp-content/plugins/gutenberg/lib/client-assets.php`.
+			 *
+			 * Adds gutenberg inline scripts that are needed so we can call `wp.blocks.getBlockTypes()`
+			 * outside of gutenberg contexts (like the post editor page), and leverage its API
+			 * on admin settings page where the editor won't be used.
+			 */
+			if ( function_exists( 'gutenberg_editor_scripts_and_styles' ) ) {
+				gutenberg_editor_scripts_and_styles( $hook_suffix );
+			}
+			wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/bu-blocks-admin.js', array( 'wp-blocks', 'wp-element', 'wp-components', 'wp-i18n', 'wp-block-library', 'wp-editor', 'wp-edit-post' ), $this->version, true );
+		}
+	}
+
+	/**
+	 * Returns admin pages that should output bu-blocks plugin scripts.
+	 *
+	 * Plugin scrits should output where the gutenberg editor is present,
+	 * such as the edit page or post screen, and also output on the
+	 * plugin settings admin page.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @return   array An array of admin page locations to output bu-blocks scripts.
+	 */
+	public function get_admin_pages_for_scripts() {
+		return array(
+			'post.php',                // The post edit screen.
+			'settings_page_bu-blocks', // The plugin settings screen.
+		);
 	}
 
 	/**
@@ -78,8 +149,8 @@ class BU_Blocks_Admin {
 			'BU Blocks',
 			'BU Blocks',
 			'manage_options',
-			'bu-blocks',
-			array( $this, 'load_block_list_table' )
+			$this->plugin_name,
+			array( $this, 'load_bu_blocks_admin_page' )
 		);
 
 		/*
@@ -88,57 +159,37 @@ class BU_Blocks_Admin {
 		 * The $page_hook_suffix can be combined with the load-($page_hook) action hook.
 		 * The callback below will be called when the respective page is loaded.
 		 *
+		 * This hook is currently not being used anywhere in the plugin, but could be used
+		 * to add screen options.
+		 *
 		 * @since    1.0.0
 		 *
 		 * @link    https://codex.wordpress.org/Plugin_API/Action_Reference/load-(page).
+		 * @link    https://www.sitepoint.com/using-wp_list_table-to-create-wordpress-admin-tables/
 		 */
-		add_action( 'load-' . $page_hook, array( $this, 'load_block_list_table_screen_options' ) );
+		add_action( 'load-' . $page_hook, array( $this, 'load_bu_blocks_admin_pages_hook' ) );
 	}
 
 	/**
-	 * Screen options for the List Table.
+	 * Callback to load screen options for settings page.
 	 *
 	 * Callback for the load-($page_hook_suffix).
 	 * Called when the plugin page is loaded.
 	 *
 	 * @since    1.0.0
 	 */
-	public function load_block_list_table_screen_options() {
-
-		// Defines areguments for screen options.
-		$arguments = array(
-			'label'   => __( 'Blocks Per Page', 'bu-blocks' ),
-			'default' => 5,
-			'option'  => 'bu_blocks_per_page',
-		);
-
-		// Adds the screen option.
-		add_screen_option( 'per_page', $arguments );
-
-		/**
-		 * Instantiates the Block List Table.
-		 *
-		 * Creating an instance here will allow the core WP_List_Table class to automatically
-		 * load the table columns in the screen options panel.
-		 */
-		$this->block_list_table = new BU\Plugins\BU_Blocks\Admin\BU_Block_List_Table( $this->block_instances );
-
+	public function load_bu_blocks_admin_pages_hook() {
 	}
 
 	/**
-	 * Display the User List Table
+	 * Displays the plugin settings page.
 	 *
 	 * Callback for the add_options_page() in the add_plugin_admin_menu() method of this class.
 	 *
 	 * @since    1.0.0
 	 */
-	public function load_block_list_table() {
-
-		// Query, filter, and sort the data.
-		$this->block_list_table->prepare_items();
-
-		// Renders the List Table.
-		include_once BU_BLOCKS_DIR . '/admin/partial-block-list-table-display.php';
+	public function load_bu_blocks_admin_page() {
+		include_once plugin_dir_path( __FILE__ ) . 'partials/settings-page.php';
 	}
 
 }
