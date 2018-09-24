3df0a399c9dc1b73b2f227ee961d8197df060a4b
Todd Milliken <milliktr@gmail.com>
2018-09-24 10:25:30 -0400

    Add more clarification to docblock comments

--- admin/js/bu-blocks-admin.js
+++ admin/js/bu-blocks-admin.js
@@ -9,11 +9,15 @@ console.log( 'Hello World, from BU Blocks' );
  * Waits until all gutenberg scripts have finished initializing,
  * so we have access to the wp.blocks object.
  *
- * Note: This promise `_wpLoadGutenbergEditor` may very likely change
- * in a future version of WP. Also, there may be a more reliable way to
- * retrieve all registered blocks than javascript in the future, such as
- * a PHP API. Since none exists, we need to enqueue gutenberg scripts to
- * detect all registered blocks so we can output them on a settings page.
+ * @link https://github.com/WordPress/gutenberg/issues/4848#issuecomment-388174948
+ *
+ * Note: This promise `_wpLoadGutenbergEditor` is a "private" function
+ * that shouldn't be relied upon, apparently. It may very likely change
+ * in a future version of WP; or there may be a more reliable way to
+ * wrap gutenberg logic with or a better way to retrieve all registered
+ * blocks than javascript in the future, such as a PHP API. Since none
+ * exists, we need to enqueue gutenberg scripts to detect all registered
+ * blocks so we can output them on a settings page.
  */
 window._wpLoadGutenbergEditor.then( function() {
 
@@ -28,8 +32,16 @@ window._wpLoadGutenbergEditor.then( function() {
      */
     console.log( wp.blocks.getBlockTypes() );
 
-    // Runs the following scripts on the post edit screen, where gutenberg is used.
+    /**
+     * Console logs the current admin page that was localized
+     * via wp_localize_script. Useful so we know if we should
+     * unregister the block because its a post-edit page,
+     * or skip unregistering blocks because its a plugin settings
+     * page.
+     */
     console.log( BU_Blocks_Settings.admin_page );
+
+    // Runs the following scripts on the post edit screen, where gutenberg is used.
     if ( 'post.php' === BU_Blocks_Settings.admin_page ) {
         /**
          * Loops through BU Blocks settings for unregistered blocks, and
