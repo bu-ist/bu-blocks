a0be94c2efdea27bc77705315036d44c44d3329f
acketon <acketon@gmail.com>
2018-10-03 16:59:29 -0400

    add KSS styleguide and an example

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,12 +42,125 @@ module.exports = function( grunt ) {
 				},
 			},
 		},
+		watch: {
+			grunt: {
+				files: [ 'Gruntfile.js' ],
+				options: {
+					reload: true
+				}
+			},
+			styles: {
+				files: [
+					'css-dev/**/*.scss',
+					'src/**/**/*.scss'
+				],
+				tasks: [ 'styles' ],
+				options: {
+					spawn: false
+				}
+			},
+			kss: {
+				files: [
+					'css-dev/**/*.hbs',
+					'src/**/*.hbs'
+				],
+				tasks: [ 'styles' ],
+				options: {
+					spawn: false
+				}
+			},
+		},
+		sass: {
+			options: {
+				outputStyle: 'compressed',
+				sourceMap: true,
+				indentType: 'space',
+				indentWidth: 2,
+				precision: '5',
+				bundleExec: true,
+				includePaths: [
+					'node_modules/normalize.scss/sass',
+					'node_modules/mathsass/dist/',
+					'node_modules/responsive-foundation/css-dev',
+					'css-dev/',
+					'src/'
+				],
+			},
+			prod: {
+				files: {
+					'style.css': 'css-dev/style.scss'
+				}
+			}
+		},
+		copy: {
+			css: {
+				options: {
+					mode: true
+				},
+				src: 'dist/blocks.style.build.css',
+				dest: '_styleguide/blocks.style.build.css'
+			},
+			kssassets: {
+				cwd: 'kss-assets',  // set working folder / root to copy
+				src: '**/*',           // copy all files and subfolders
+				dest: '_styleguide/kss-assets',    // destination folder
+				expand: true           // required when using cwd
+			}
+		},
+		kss: {
+			options: {
+				title: 'BU Blocks Style Guide',
+				builder: 'node_modules/id-kss-builder',
+				css: [
+				  "/blocks.style.build.css",
+				],
+				js: [
+				  "/script.js",
+				],
+				extend: 'node_modules/id-kss-builder/extend',
+				gitURL: 'https://github.com/bu-ist/bu-blocks/',
+				gitURLCSSDEV: 'https://github.com/bu-ist/bu-blocks/tree/develop/src',
+				exampleStylesheetURL: 'https://www.bu.edu/wp-content/themes/responsive-framework-2-x/style.min.css'
+			},
+			dist: {
+				src: [
+					['css-dev', 'src']
+				],
+				dest: '_styleguide'
+			}
+		},
+
+		browserSync: {
+			bsFiles: {
+				src : '_styleguide/*.html'
+			},
+			options: {
+				watchTask: true,
+				server: {
+					baseDir: "./_styleguide"
+				}
+			}
+		}
 	} );
 
+	// 3. Where we tell Grunt we plan to use this plug-in.
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-contrib-copy' );
+	grunt.loadNpmTasks( 'grunt-sass' );
+	grunt.loadNpmTasks( 'grunt-kss' );
+	grunt.loadNpmTasks( 'grunt-browser-sync' );
 	grunt.loadNpmTasks( 'grunt-wp-i18n' );
 	grunt.loadNpmTasks( 'grunt-wp-readme-to-markdown' );
+
+
 	grunt.registerTask( 'i18n', [ 'addtextdomain', 'makepot' ] );
 	grunt.registerTask( 'readme', [ 'wp_readme_to_markdown' ] );
+
+	// 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+	grunt.registerTask( 'install',  [ 'build' ] );
+	grunt.registerTask( 'styles',   [ 'sass', 'kss', 'copy:css', 'copy:kssassets' ] );
+	grunt.registerTask( 'build',    [ 'styles', 'kss', 'copy:css', 'copy:kssassets'  ] );
+	grunt.registerTask( 'default',  [ 'browserSync', 'watch' ] );
 
 	grunt.util.linefeed = '\n';
 };
