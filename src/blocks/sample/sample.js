34dd3084426443d6eb039b03c536ef073ebbe5f0
Charlie Guerrero <gcharlie@bu.edu>
2018-10-11 16:04:22 -0400

    left over sample global repalce

--- src/blocks/sample/sample.js
+++ src/blocks/sample/sample.js
@@ -1,5 +1,5 @@
 /**
- * BLOCK: bu-pullquote-cgb
+ * BLOCK: bu-sample-cgb
  *
  * Registering a basic block with Gutenberg.
  * Simple block, renders and saves the same content without any interactivity.
