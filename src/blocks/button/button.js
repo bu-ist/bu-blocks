b339f64186881832919b8c20f7e6836430f0cd3c
crystalms <cseitz@bu.edu>
2018-11-02 16:59:07 -0400

    Update block naming

--- src/blocks/button/button.js
+++ src/blocks/button/button.js
@@ -1,5 +1,5 @@
 /**
- * BLOCK: bu-aside-cgb
+ * BLOCK: bu-button-cgb
  *
  * Registering a basic block with Gutenberg.
  * Simple block, renders and saves the same content without any interactivity.
