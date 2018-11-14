d5a23028a1300e4cca7064ca649d819255448b14
philcable <pbcable@bu.edu>
2018-11-14 11:04:08 -0800

    Register the Aside block
    Just the basics for now.

--- src/blocks/aside/aside.js
+++ src/blocks/aside/aside.js
@@ -1,10 +1,43 @@
 /**
  * BLOCK: bu-aside-cgb
  *
- * Registering a basic block with Gutenberg.
- * Simple block, renders and saves the same content without any interactivity.
+ * A container for related information that accepts image,
+ * headline, paragraph, and button blocks as children.
  */
 
-//  Import CSS.
+// Import CSS.
 import './style.scss';
 import './editor.scss';
+
+// WordPress dependencies.
+const { __ } = wp.i18n;
+const { registerBlockType } = wp.blocks;
+const { InnerBlocks } = wp.editor;
+
+// Register the block.
+registerBlockType( 'editorial/aside', {
+
+	title: __( 'Aside' ),
+	description: __( 'Add an aside with related information. Accepts image, headline, paragraph, and button blocks as children.' ),
+	icon: 'format-aside',
+	category: 'bu-editorial',
+	supports: {
+		align: [ 'left', 'right' ],
+	},
+
+	edit() {
+		return (
+			<aside>
+				<InnerBlocks />
+			</aside>
+		);
+	},
+
+	save() {
+		return (
+			<aside>
+				<InnerBlocks.Content />
+			</aside>
+		);
+	},
+} );
