f2649f2098baac74e16d71db9954629353a9ca62
Charlie Guerrero <gcharlie@bu.edu>
2018-11-19 08:18:28 -0500

    change nameing

--- src/blocks/leadin/leadin.js
+++ src/blocks/leadin/leadin.js
@@ -1,5 +1,5 @@
 /**
- * BLOCK: bu-pullquote-cgb
+ * BLOCK: bu-leadin-cgb
  *
  * Registering a basic block with Gutenberg.
  * Simple block, renders and saves the same content without any interactivity.
