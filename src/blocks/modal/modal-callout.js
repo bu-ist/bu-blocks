36310380789dc54aa6190d2954b4fd11d82e35cf
philcable <pbcable@bu.edu>
2018-11-20 10:04:21 -0800

    Add callout block image functionality
    This is borrowed from the default Cover block. I think it makes
    sense to use the same workflow in this case - or at the very least,
    using it will provide some consistency of experience.

--- src/blocks/modal/modal-callout.js
+++ src/blocks/modal/modal-callout.js
@@ -7,7 +7,9 @@
 // WordPress dependencies.
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
-const { InnerBlocks } = wp.editor;
+const { Fragment } = wp.element;
+const { Toolbar, IconButton } = wp.components;
+const { BlockControls, InnerBlocks, MediaPlaceholder, MediaUpload } = wp.editor;
 
 // Register the block.
 registerBlockType( 'editorial/modal-callout', {
@@ -20,31 +22,126 @@ registerBlockType( 'editorial/modal-callout', {
 	supports: {
 		inserter: false,
 	},
+	attributes: {
+		url: {
+			attribute: 'src',
+			selector: '.banner-placeholder',
+			type: 'string',
+		},
+		id: {
+			type: 'number',
+		},
+		alt: {
+			attribute: 'alt',
+			selector: '.banner-placeholder',
+			type: 'string',
+		},
+	},
 
-	edit( { className } ) {
+	edit( { className, attributes, setAttributes } ) {
+		const { url, id, alt } = attributes;
+
+		const onSelectMedia = ( media ) => {
+			if ( ! media || ! media.url ) {
+				setAttributes( { url: undefined, id: undefined } );
+				return;
+			}
+
+			setAttributes( {
+				url: media.url,
+				id: media.id,
+			} );
+		};
+
+		const controls = (
+			<Fragment>
+				<BlockControls>
+					{ !! url && (
+						<Toolbar>
+							<MediaUpload
+								onSelect={ onSelectMedia }
+								allowedTypes={ [ 'image' ] }
+								value={ id }
+								render={ ( { open } ) => (
+									<IconButton
+										className="components-toolbar__control"
+										label={ __( 'Edit image' ) }
+										icon="edit"
+										onClick={ open }
+									/>
+								) }
+							/>
+						</Toolbar>
+					) }
+				</BlockControls>
+			</Fragment>
+		);
+
+		if ( ! url ) {
+			return (
+				<Fragment>
+					{ controls }
+					<MediaPlaceholder
+						icon='format-image'
+						className={ className }
+						labels={ {
+							title: __( 'Modal callout' ),
+							instructions: __( 'Drag an image, upload a new one or select a file from your library.' ),
+						} }
+						onSelect={ onSelectMedia }
+						accept="image/*"
+						allowedTypes={ [ 'image '] }
+					/>
+				</Fragment>
+			);
+		}
+
 		return (
-			<div className={ className }>
-				<div class="wp-block-editorial-modal-media">
-					<figure class="wp-block-editorial-modal-image">
-
-					</figure>
-				</div>
-				<div class="wp-block-editorial-modal-callout-content">
-					<div class="modal-valign">
-						<InnerBlocks templateLock={ false } />
-						<p><a href="#one" class="js-bu-block-modal-trigger-overlay button">Explore</a></p>
+			<Fragment>
+				{ controls }
+				<div className={ className }>
+					<div class="wp-block-editorial-modal-media">
+						<figure class="wp-block-editorial-modal-image">
+							<img
+								className="banner-placeholder"
+								src={ url }
+								alt={ ( alt ) ? alt : '' }
+							/>
+						</figure>
+					</div>
+					<div class="wp-block-editorial-modal-callout-content">
+						<div class="modal-valign">
+							<InnerBlocks templateLock={ false } />
+							<p><a href="#one" class="js-bu-block-modal-trigger-overlay button">Explore</a></p>
+						</div>
 					</div>
 				</div>
-			</div>
+			</Fragment>
 		);
 	},
 
-	save() {
+	save( { attributes } ) {
+		const { url, alt } = attributes;
+
+		const cardImage = ( src, alt ) => {
+			if ( ! src ) {
+				return null;
+			}
+
+			return (
+				<img
+					className="banner-placeholder"
+					src={ src }
+					alt={ ( alt ) ? alt : '' }
+				/>
+			);
+		};
+
 		return (
 			<div>
 				<div class="wp-block-editorial-modal-media">
 					<figure class="wp-block-editorial-modal-image">
-
+						{ cardImage( url, alt ) }
 					</figure>
 				</div>
 				<div class="wp-block-editorial-modal-callout-content">
