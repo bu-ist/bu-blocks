35ca2a22b59e4b4ab2757b3ccff11deac894651a
philcable <pbcable@bu.edu>
2018-11-20 11:07:54 -0800

    Add inspector controls for setting the modal background

--- src/blocks/modal/modal.js
+++ src/blocks/modal/modal.js
@@ -15,7 +15,9 @@ import './modal-content';
 // WordPress dependencies.
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
-const { InnerBlocks } = wp.editor;
+const { Fragment } = wp.element;
+const { PanelBody, SelectControl } = wp.components;
+const { InspectorControls, InnerBlocks } = wp.editor;
 
 // The current publication owner.
 const publicationClass = document.getElementById( 'bu_publication_owner' ).value;
@@ -27,26 +29,58 @@ const template = [ [ 'editorial/modal-callout' ], [ 'editorial/modal-content' ] 
 registerBlockType( 'editorial/modal', {
 
 	title: __( 'Modal' ),
-	description: __( 'A block with a call to action for opening a modal with supplemental or complementary information.' ),
+	description: __( 'A block with a callout for opening a modal with supplemental or complementary information.' ),
 	icon: 'admin-page',
 	category: 'bu-editorial',
+	attributes: {
+		background: {
+			type: 'string',
+			default: '',
+		},
+	},
 	publicationClassName: publicationClass + '-block-modal',
 
-	edit( { className } ) {
+	edit( { attributes, setAttributes, className } ) {
+		const { background } = attributes;
+		const classList = [ className, background ].join( ' ' ).trim();
+
 		return (
-			<aside className={ className }>
-				<InnerBlocks
-					allowedBlocks={ allowedBlocks }
-					template={ template }
-					templateLock='all'
-				/>
-			</aside>
+			<Fragment>
+				<InspectorControls>
+					<PanelBody title={ __( 'Modal Background Settings' ) }>
+						<SelectControl
+							label={ __( 'Background' ) }
+							value={ background || '' }
+							onChange={ value => setAttributes( { background: value } ) }
+							options={ [
+								{ value: '', label: __( 'Default' ) },
+								{ value: 'has-light-theme', label: __( 'Light' ) },
+								{ value: 'has-dark-theme', label: __( 'Dark' ) },
+								{ value: 'has-primary-theme', label: __( 'Primary' ) },
+								{ value: 'has-secondary-theme', label: __( 'Secondary' ) },
+								{ value: 'has-tertiary-theme', label: __( 'Tertiary' ) },
+								{ value: 'has-quaternary-theme', label: __( 'Quaternary' ) },
+							] }
+						/>
+					</PanelBody>
+				</InspectorControls>
+				<aside className={ classList }>
+					<InnerBlocks
+						allowedBlocks={ allowedBlocks }
+						template={ template }
+						templateLock='all'
+					/>
+				</aside>
+			</Fragment>
 		);
 	},
 
-	save() {
+	save( { attributes, className } ) {
+		const { background } = attributes;
+		const classList = [ className, background, 'js-bu-block-modal' ].join( ' ' ).trim();
+
 		return (
-			<aside>
+			<aside className={ classList }>
 				<InnerBlocks.Content />
 			</aside>
 		);
