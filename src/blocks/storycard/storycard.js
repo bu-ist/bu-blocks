4d904a9fb48ae26212c613a42e78fb50ef3a7443
acacase <acacase@bu.edu>
2018-12-07 16:24:35 -0500

    updated story card

--- src/blocks/storycard/storycard.js
+++ src/blocks/storycard/storycard.js
@@ -1,5 +1,5 @@
 /**
- * BLOCK: bu-sample-cgb
+ * BLOCK: bu-storycard-cgb
  *
  * Registering a basic block with Gutenberg.
  * Simple block, renders and saves the same content without any interactivity.
