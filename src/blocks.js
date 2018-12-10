1cc96d540a818c62864cc9984c507672b553d148
acacase <acacase@bu.edu>
2018-12-10 17:51:03 -0500

    simplified related stories
    - removed components
    - moved styles

--- src/blocks.js
+++ src/blocks.js
@@ -18,6 +18,4 @@ import './blocks/introparagraph/introparagraph.js';
 import './blocks/leadin/leadin.js';
 import './blocks/pullquote/pullquote.js';
 import './blocks/button/button.js';
-import './blocks/storycard/storycard.js';
-import './blocks/storylist/storylist.js';
 import './blocks/relatedstories/relatedstories.js';
