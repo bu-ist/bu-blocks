c38be659395275087ca25f283e97d4d9ec04d6f3
philcable <pbcable@bu.edu>
2019-01-08 10:21:27 -0800

    Fix publication class typo
    Lazy...

--- src/blocks/photoessay/photoessay.js
+++ src/blocks/photoessay/photoessay.js
@@ -46,7 +46,7 @@ registerBlockType( 'editorial/photoessay', {
 	description: __( 'Insert a row of photos with optional layouts.' ),
 	icon: <SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><Path fill="#c00" d="M19 7h-1V5h-4v2h-4V5H6v2H5c-1.1 0-2 .9-2 2v10h18V9c0-1.1-.9-2-2-2zm0 10H5V9h14v8z"></Path></SVG>,
 	category: 'bu-editorial',
-	publicationClassName: publicationClass + '-block-editorial-introparagraph',
+	publicationClassName: publicationClass + '-block-editorial-photoessay',
 	attributes: {
 		layout: {
 			type: 'string',
