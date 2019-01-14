b70c9b8ad8714b60e3bfe97e84039ee34c80d631
philcable <pbcable@bu.edu>
2019-01-14 10:04:24 -0800

    Split some control options into separate functions, add inline comments
    This should help improve readability :)

--- src/blocks/leadin/edit.js
+++ src/blocks/leadin/edit.js
@@ -25,6 +25,7 @@ class BULeadinEdit extends Component {
 	}
 
 	render() {
+		// Get the block properties.
 		const {
 			attributes,
 			themeColor,
@@ -34,6 +35,7 @@ class BULeadinEdit extends Component {
 			isSelected,
 		} = this.props;
 
+		// Get the block attributes.
 		const {
 			backgroundId,
 			head,
@@ -69,7 +71,7 @@ class BULeadinEdit extends Component {
 
 		const primaryTermInput = document.getElementById( '_bu_prepress_primary_term_select' );
 
-		// Not all themes have primary term support.
+		// Check that the primary term input exists, as the feature may not be supported.
 		if ( primaryTermInput ) {
 			if ( primaryTermInput.value ) {
 				setAttributes( { primaryTerm: primaryTermInput.value } );
@@ -80,6 +82,108 @@ class BULeadinEdit extends Component {
 			} );
 		}
 
+		// Return the background media positioning controls if a background is set.
+		const mediaPositioningControls = () => {
+			if ( ! backgroundId ) {
+				return null;
+			}
+
+			return (
+				<PanelBody title={ __( 'Media Positioning' ) } initialOpen={ false }>
+					<SelectControl
+						label={ __( 'Media Focal Point' ) }
+						value={ imageFocus }
+						onChange={ value => setAttributes( { imageFocus: value } ) }
+						options={ [
+							{ value: 'left-top', label: __( 'Left Top' ) },
+							{ value: 'left-middle', label: __( 'Left Center' ) },
+							{ value: 'left-bottom', label: __( 'Left Bottom' ) },
+							{ value: 'center-top', label: __( 'Center Top' ) },
+							{ value: 'center-middle', label: __( 'Center' ) },
+							{ value: 'center-bottom', label: __( 'Center Bottom' ) },
+							{ value: 'right-top', label: __( 'Right Top' ) },
+							{ value: 'right-middle', label: __( 'Right Center' ) },
+							{ value: 'right-bottom', label: __( 'Right Bottom' ) },
+						] }
+					/>
+				</PanelBody>
+			);
+		};
+
+		// Return the text positioning controls if the 'Image with Text Overlay' style is set.
+		const textPositioningControls = () => {
+			if ( ! isStyleTextOverImage ) {
+				return null;
+			}
+
+			return (
+				<Fragment>
+					<SelectControl
+						label={ __( 'Horizontal Text Positioning' ) }
+						value={ textPositionX }
+						onChange={ value => setAttributes( { textPositionX: value } ) }
+						options={ [
+							{ value: 'x-left', label: __( 'Left' ) },
+							{ value: 'x-center', label: __( 'Center' ) },
+							{ value: 'x-right', label: __( 'Right' ) }
+						] }
+					/>
+					<SelectControl
+						label={ __( 'Vertical Text Positioning' ) }
+						value={ textPositionY }
+						onChange={ value => setAttributes( { textPositionY: value } ) }
+						options={ [
+							{ value: 'y-top', label: __( 'Top' ) },
+							{ value: '', label: __( 'Center' ) },
+							{ value: 'y-bottom', label: __( 'Bottom' ) }
+						] }
+					/>
+				</Fragment>
+			);
+		};
+
+		// Return layout controls for the 'Vertical Image and Text Side By Side' style.
+		const sideBySideLayoutControls = () => {
+			if ( ! className.includes( 'is-style-side-by-side' ) ) {
+				return null;
+			}
+
+			return (
+				<Fragment>
+					<CheckboxControl
+						label={ __( 'Flip Order' ) }
+						checked={ flip }
+						onChange={ ( flip ) => { setAttributes( { flip } ) } }
+					/>
+					<CheckboxControl
+						label={ __( 'Wide Layout' ) }
+						checked={ wide }
+						onChange={ ( wide ) => { setAttributes( { wide } ) } }
+					/>
+				</Fragment>
+			);
+		};
+
+		// Return layout options if specific styles are set.
+		const layoutControls = () => {
+			if ( ! ( isStyleEmphasisOnText || isStyleTextOverImage || isStyleSideBySide ) ) {
+				return null;
+			}
+
+			return (
+				<PanelBody title={ __( 'Layout Options' ) }>
+					{ sideBySideLayoutControls() }
+					{ textPositioningControls() }
+					<CheckboxControl
+						label={ __( 'Boxed Text' ) }
+						checked={ box }
+						onChange={ ( box ) => { setAttributes( { box } ) } }
+					/>
+				</PanelBody>
+			);
+		};
+
+		// Return the block editing interface.
 		return (
 			<Fragment>
 				<div className={ classes }>
@@ -116,75 +220,8 @@ class BULeadinEdit extends Component {
 					</div>
 				</div>
 				<InspectorControls>
-					{ ( backgroundId ) && (
-						<PanelBody title={ __( 'Media Positioning' ) } initialOpen={ false }>
-							<SelectControl
-								label={ __( 'Media Focal Point' ) }
-								value={ imageFocus }
-								onChange={ value => setAttributes( { imageFocus: value } ) }
-								options={ [
-									{ value: 'left-top', label: __( 'Left Top' ) },
-									{ value: 'left-middle', label: __( 'Left Center' ) },
-									{ value: 'left-bottom', label: __( 'Left Bottom' ) },
-									{ value: 'center-top', label: __( 'Center Top' ) },
-									{ value: 'center-middle', label: __( 'Center' ) },
-									{ value: 'center-bottom', label: __( 'Center Bottom' ) },
-									{ value: 'right-top', label: __( 'Right Top' ) },
-									{ value: 'right-middle', label: __( 'Right Center' ) },
-									{ value: 'right-bottom', label: __( 'Right Bottom' ) },
-								] }
-							/>
-						</PanelBody>
-					) }
-					{ ( isStyleEmphasisOnText || isStyleTextOverImage || isStyleSideBySide ) && (
-						<PanelBody title={ __( 'Layout Options' ) }>
-							{ isStyleTextOverImage && (
-								<SelectControl
-									label={ __( 'Horizontal Text Positioning' ) }
-									value={ textPositionX }
-									onChange={ value => setAttributes( { textPositionX: value } ) }
-									options={ [
-										{ value: 'x-left', label: __( 'Left' ) },
-										{ value: 'x-center', label: __( 'Center' ) },
-										{ value: 'x-right', label: __( 'Right' ) }
-									] }
-								/>
-							) }
-							{ isStyleTextOverImage && (
-								<SelectControl
-									label={ __( 'Vertical Text Positioning' ) }
-									value={ textPositionY }
-									onChange={ value => setAttributes( { textPositionY: value } ) }
-									options={ [
-										{ value: 'y-top', label: __( 'Top' ) },
-										{ value: '', label: __( 'Center' ) },
-										{ value: 'y-bottom', label: __( 'Bottom' ) }
-									] }
-								/>
-							) }
-							{ ( isStyleEmphasisOnText || isStyleTextOverImage || isStyleSideBySide ) && (
-								<CheckboxControl
-									label={ __( 'Boxed Text' ) }
-									checked={ box }
-									onChange={ ( box ) => { setAttributes( { box } ) } }
-								/>
-							) }
-							{ className.includes( 'is-style-side-by-side' ) && (
-								<CheckboxControl
-									label={ __( 'Flip Order' ) }
-									checked={ flip }
-									onChange={ ( flip ) => { setAttributes( { flip } ) } }
-								/>
-							) }
-							{ className.includes( 'is-style-side-by-side' ) && (
-								<CheckboxControl
-									label={ __( 'Wide Layout' ) }
-									checked={ wide }
-									onChange={ ( wide ) => { setAttributes( { wide } ) } }
-								/>
-							) }
-						</PanelBody>
-					) }
+					{ mediaPositioningControls() }
+					{ layoutControls() }
 					<PanelColorSettings
 						title={ __( 'Color Settings' ) }
 						initialOpen={ false }
