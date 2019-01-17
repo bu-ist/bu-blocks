8c415fb97f45f6c91d986a357945b79cb0a06de4
Jeremy Felt <jeremy.felt@gmail.com>
2019-01-17 08:53:27 -0800

    Add the custom YARPP endpoint for finding related posts
    This was transferred from BU Prepress during the development of:
    
    https://github.com/bu-ist/bu-prepress/pull/45/

---
+++ src/includes/yarpp-endpoint.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Adds a REST API endpoint for the YARPP plugin.
+ *
+ * @link       www.bu.edu/interactive-design/
+ * @since      0.1.0
+ *
+ * @package    BU_Blocks
+ * @subpackage BU_Blocks/src/includes
+ */
+
+namespace BU\Plugins\BU_Blocks\YARPPEndpoint;
+
+add_action( 'rest_api_init', __NAMESPACE__ . '\register_route' );
+
+/**
+ * Register the REST API route used to provide related posts to the
+ * related stories block.
+ */
+function register_route() {
+	register_rest_route(
+		'bu-yarpp/v1',
+		'related',
+		array(
+			'methods'  => 'GET',
+			'callback' => __NAMESPACE__ . '\rest_response',
+		)
+	);
+}
+
+/**
+ * Provide a list of related posts from YARPP if the plugin
+ * is enabled.
+ *
+ * @param \WP_Request $request The incoming REST API request object.
+ * @return array A list of post IDs.
+ */
+function rest_response( $request ) {
+	if ( function_exists( 'yarpp_get_related' ) ) {
+		$post_id = $request->get_param( 'post_id' );
+		$posts   = yarpp_get_related( array(), $post_id );
+		$posts   = wp_list_pluck( $posts, 'ID' );
+	} else {
+		return new \WP_Error( 'yarpp_disabled', 'The YARPP plugin is not available', array( 'status' => 501 ) );
+	}
+
+	return $posts;
+}
