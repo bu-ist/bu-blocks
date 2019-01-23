8c873fdcccb3400ce2977e0a20ff6379100c2f03
acketon <acketon@gmail.com>
2019-01-23 09:38:49 -0500

    add functions to setup initial slideshow state, and basic next/prev functions for images and captions

--- src/blocks/slideshow/frontend.js
+++ src/blocks/slideshow/frontend.js
@@ -2,8 +2,7 @@ bu_blocks.slideshow = (function() {
 	var slideshowBlocks = [];
 	var $body = document.getElementsByTagName('body')[0];
 
-	//findElements = function() {
-	function findElements() {
+	var findElements = function() {
 		//find all the blocks
 		var elements = document.getElementsByClassName('js-bu-blocks-slideshow');
 
@@ -25,28 +24,37 @@ bu_blocks.slideshow = (function() {
 				//get media track
 				block.mediatrack = elements[i].getElementsByClassName('js-bu-blocks-slideshow-media-track')[0];
 
+				//get media items
+				block.mediatrackitems = elements[i].getElementsByClassName('js-bu-blocks-slideshow-media-track-item');
+
 				//get caption track
 				block.captiontrack = elements[i].getElementsByClassName('js-bu-blocks-slideshow-caption-track')[0];
+
+				//get caption items
+				block.captiontrackitems = elements[i].getElementsByClassName('js-bu-blocks-slideshow-caption-item');
 
 				//for each one found store as object in the array
 				slideshowBlocks.push(block);
 			}
+			//console.log(slideshowBlocks);
 		}
 	};
 
 
-	setupHandlers = function() {
+	var setupHandlers = function() {
 		if (slideshowBlocks.length > 0) {
 			for ( i = 0; i < slideshowBlocks.length; i++ ) {
-				slideshowBlocks[i].backBtn.addEventListener("click", function(e){
+				var block = slideshowBlocks[i];
+
+				block.backBtn.addEventListener("click", function(e){
 					e.preventDefault();
-					alert("back clicked");
+					prevItem( block );
 				});
-				slideshowBlocks[i].forwardBtn.addEventListener("click", function(e){
+				block.forwardBtn.addEventListener("click", function(e){
 					e.preventDefault();
-					alert("forward clicked");
+					nextItem( block );
 				});
-				slideshowBlocks[i].captionBtn.addEventListener("click", function(e) {
+				block.captionBtn.addEventListener("click", function(e) {
 					e.preventDefault();
 					alert("caption clicked");
 				});
@@ -54,12 +62,79 @@ bu_blocks.slideshow = (function() {
 		}
 	};
 
-	slideshowInit = function() {
+
+	var setupMediaTrack = function() {
+		for ( i = 0; i < slideshowBlocks.length; i++ ) {
+			var block = slideshowBlocks[i];
+			if ( block.mediatrack && block.mediatrackitems ) {
+				//set currentItem variable
+				block.currentItem = 0;
+
+				//store number of items
+				block.itemslength = block.mediatrackitems.length;
+
+
+				//set default width
+				block.mediatrack.style.width = 'calc(' + block.itemslength + ' * 100%)';
+				//set start position to first image
+				block.mediatrack.style.left = '0%';
+
+				//for each image calculate the width
+				for ( var i = 0; i < block.mediatrackitems.length; i++ ) {
+					block.mediatrackitems[i].style.width = 'calc(1/' + block.itemslength + ' * 100% - 2px)';
+				}
+
+			}
+
+
+			//setup the caption track starting widths and postion
+			if( block.captiontrack && block.captiontrackitems ) {
+				//set default width
+				block.captiontrack.style.width = 'calc(' + block.itemslength + ' * 100%)';
+				//set start position to first caption
+				block.captiontrack.style.left = '0%';
+
+				//for each caption calculate the width
+				for ( var i = 0; i < block.captiontrackitems.length; i++ ) {
+					block.captiontrackitems[i].style.width = 'calc(1/' + block.itemslength + ' * 100%)';
+				}
+			}
+		}
+
+	};
+
+
+	var nextItem = function( block ) {
+		if( block.currentItem === block.itemslength - 1 ) {
+			//can't go next anymore
+		} else {
+			block.currentItem = block.currentItem + 1;
+			block.mediatrack.style.left = block.currentItem * -100 + '%';
+			block.captiontrack.style.left = block.currentItem * -100 + '%';
+		}
+	};
+
+	var prevItem = function( block ) {
+		if( block.currentItem === 0 ) {
+			//do nothing can't go back more
+		} else {
+			block.currentItem = block.currentItem - 1;
+			block.mediatrack.style.left = block.currentItem * -100 + '%';
+			block.captiontrack.style.left = block.currentItem * -100 + '%';
+		}
+	};
+
+
+
+
+	var slideshowInit = function() {
 		//find the elements
 		findElements();
 
 		//setup handlers
 		setupHandlers();
+
+		setupMediaTrack();
 	};
 
 	//start on dom ready (ie8+)
