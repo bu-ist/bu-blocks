928124751de5766c4dbcfe3d20a637cad696d1fb
Charlie Guerrero <gcharlie@bu.edu>
2019-01-24 12:59:30 -0500

    add functionality
    caption sizing, active media and caption toggling

--- src/blocks/slideshow/frontend.js
+++ src/blocks/slideshow/frontend.js
@@ -18,6 +18,13 @@ bu_blocks.slideshow = (function() {
 				//get forward btn
 				block.forwardBtn = elements[i].getElementsByClassName('js-bu-blocks-slideshow-forward-btn')[0];
 
+				//get onmedia back btn
+				block.backMediaBtn = elements[i].getElementsByClassName('js-bu-blocks-slideshow-back-onmedia-btn')[0];
+				//get onmedia forward btn
+				block.forwardMediaBtn = elements[i].getElementsByClassName('js-bu-blocks-slideshow-forward-onmedia-btn')[0];
+				//get ontrack forward btn
+				block.forwardTrackBtn = elements[i].getElementsByClassName('js-bu-blocks-slideshow-forward-ontrack-btn')[0];
+
 				//get caption btn
 				block.captionBtn = elements[i].getElementsByClassName('js-bu-blocks-slideshow-caption-btn')[0];
 
@@ -32,11 +39,29 @@ bu_blocks.slideshow = (function() {
 
 				//get caption items
 				block.captiontrackitems = elements[i].getElementsByClassName('js-bu-blocks-slideshow-caption-item');
+				//get caption items
+				block.captionContainer = elements[i].getElementsByClassName('js-bu-blocks-slideshow-caption-container')[0];
 
 				//for each one found store as object in the array
 				slideshowBlocks.push(block);
+			} 
+
+			// set active states on items and captions
+			for ( i = 0; i < block.mediatrackitems.length; i++) {
+				block.mediatrackitems[i].active = false;
+				block.captiontrackitems[i].active = false;
 			}
-			//console.log(slideshowBlocks);
+			// set first to active true
+			block.mediatrackitems[0].active = true;
+			block.captiontrackitems[0].active = true;
+			for (var i = block.mediatrackitems.length - 1; i >= 0; i--) {
+				// set active classes
+				if (block.mediatrackitems[i].active === true) {
+					block.mediatrackitems[0].classList.toggle("bu-blocks-slideshow-media-track-item-active");
+					block.captiontrackitems[0].classList.toggle("bu-blocks-slideshow-media-track-item-active");
+				}
+			}
+			block.captionContainer.collapsed = true;
 		}
 	};
 
@@ -49,14 +74,32 @@ bu_blocks.slideshow = (function() {
 				block.backBtn.addEventListener("click", function(e){
 					e.preventDefault();
 					prevItem( block );
+					sizingCaption( block );
+				});
+				block.backMediaBtn.addEventListener("click", function(e){
+					e.preventDefault();
+					prevItem( block );
+					sizingCaption( block );
 				});
 				block.forwardBtn.addEventListener("click", function(e){
 					e.preventDefault();
 					nextItem( block );
+					sizingCaption( block );
+				});
+				block.forwardMediaBtn.addEventListener("click", function(e){
+					e.preventDefault();
+					nextItem( block );
+					sizingCaption( block );
+				});
+				block.forwardTrackBtn.addEventListener("click", function(e){
+					e.preventDefault();
+					nextItem( block );
+					sizingCaption( block );
 				});
 				block.captionBtn.addEventListener("click", function(e) {
 					e.preventDefault();
-					alert("caption clicked");
+					toggleCaption( block );
+					sizingCaption( block );
 				});
 			}
 		}
@@ -83,7 +126,6 @@ bu_blocks.slideshow = (function() {
 				for ( var i = 0; i < block.mediatrackitems.length; i++ ) {
 					block.mediatrackitems[i].style.width = 'calc(1/' + block.itemslength + ' * 100% - 2px)';
 				}
-
 			}
 
 
@@ -111,6 +153,20 @@ bu_blocks.slideshow = (function() {
 			block.currentItem = block.currentItem + 1;
 			block.mediatrack.style.left = block.currentItem * -100 + '%';
 			block.captiontrack.style.left = block.currentItem * -100 + '%';
+			//move over active states
+			for (var i = block.mediatrackitems.length - 1; i >= 0; i--) {
+				if (block.mediatrackitems[i].active === true) {
+					block.mediatrackitems[i].active = false;
+					block.mediatrackitems[i].classList.toggle("bu-blocks-slideshow-media-track-item-active");
+					block.mediatrackitems[i + 1].active = true;
+					block.mediatrackitems[i + 1].classList.toggle("bu-blocks-slideshow-media-track-item-active");
+
+					block.captiontrackitems[i].active = false;
+					block.captiontrackitems[i].classList.toggle("bu-blocks-slideshow-media-track-item-active");
+					block.captiontrackitems[i + 1].active = true;
+					block.captiontrackitems[i + 1].classList.toggle("bu-blocks-slideshow-media-track-item-active");
+				}
+			}
 		}
 	};
 
@@ -121,6 +177,40 @@ bu_blocks.slideshow = (function() {
 			block.currentItem = block.currentItem - 1;
 			block.mediatrack.style.left = block.currentItem * -100 + '%';
 			block.captiontrack.style.left = block.currentItem * -100 + '%';
+			//move over active states
+			for (var i = 0; i < block.mediatrackitems.length; i++) {
+				if (block.mediatrackitems[i].active === true) {
+					block.mediatrackitems[i].active = false;
+					block.mediatrackitems[i].classList.toggle("bu-blocks-slideshow-media-track-item-active");
+					block.mediatrackitems[i - 1].active = true;
+					block.mediatrackitems[i - 1].classList.toggle("bu-blocks-slideshow-media-track-item-active");
+
+					block.captiontrackitems[i].active = false;
+					block.captiontrackitems[i].classList.toggle("bu-blocks-slideshow-media-track-item-active");
+					block.captiontrackitems[i - 1].active = true;
+					block.captiontrackitems[i - 1].classList.toggle("bu-blocks-slideshow-media-track-item-active");
+				}
+			}
+		}
+	};
+
+	var toggleCaption = function( block ) {
+		// Toggle the collapsed class
+		block.captionContainer.classList.toggle("bu-blocks-slideshow-caption-container-collapsed");
+		block.captionContainer.collapsed = !block.captionContainer.collapsed;
+	};
+
+	var sizingCaption = function( block ) {
+		// look for active caption and grab height
+		if (block.captionContainer.collapsed == false) {
+			for (var i = 0; i < block.captiontrackitems.length; i++) {
+				if (block.captiontrackitems[i].active === true) {
+					block.captiontrack.style.maxHeight = block.captiontrackitems[i].offsetHeight + 'px';
+				}
+			}
+		}
+		else {
+			block.captiontrack.style.maxHeight = '3em';
 		}
 	};
 
