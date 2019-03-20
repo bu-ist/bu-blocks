06028ce066709610679b0da64506d4231a739670
crystalms <cseitz@bu.edu>
2019-03-20 14:10:34 -0400

    Add base styles and markup for drawer block

---
+++ src/blocks/drawer/drawer.js
@@ -0,0 +1,9 @@
+/**
+ * BLOCK: bu-editorial-drawer
+ *
+ * A block that works a bit like BU Collapsible to open and close a drawer of content.
+ */
+
+// Import CSS.
+import './style.scss';
+import './editor.scss';
