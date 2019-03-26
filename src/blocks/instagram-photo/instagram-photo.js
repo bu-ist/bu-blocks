4b883bc5f64f5be3be362c5ef55a9e30610ecafe
crystalms <cseitz@bu.edu>
2019-03-26 16:59:13 -0400

    Add instagram photo markup and files

---
+++ src/blocks/instagram-photo/instagram-photo.js
@@ -0,0 +1,9 @@
+/**
+ * BLOCK: instagram-photo
+ *
+ * A block that displays square photos from Instagram. Block supports either 1 image & caption, or a mini gallery with multiple photos and a Title for the overall gallery.
+ */
+
+// Import CSS.
+import './style.scss';
+import './editor.scss';
