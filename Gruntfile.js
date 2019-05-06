6a413d8e124c0b7bd18885defcfc1abd0e1616a5
ashleykolodziej <kolodz10@bu.edu>
2019-05-06 16:30:48 -0400

    Add initial blocks support for clicktotweet

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,7 +71,8 @@ module.exports = function( grunt ) {
 			kss: {
 				files: [
 					'src/**/*.hbs',
-					'src/**/**/*.hbs'
+					'src/**/**/*.hbs',
+					'src/**/**/*.scss'
 				],
 				tasks: [ 'styles' ],
 				options: {
--- src/blocks.js
+++ src/blocks.js
@@ -24,3 +24,4 @@ import './blocks/photoessay/photoessay.js';
 import './blocks/button/button.js';
 import './blocks/relatedstories/relatedstories.js';
 import './blocks/listicle/listicle.js';
+import './blocks/clicktotweet/clicktotweet.js';
