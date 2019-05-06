6b11a456dc04ca3c187c2dbe2ad4778e4267b72e
ashleykolodziej <kolodz10@bu.edu>
2019-05-06 14:53:58 -0400

    Run KSS on subfolders change

--- Gruntfile.js
+++ Gruntfile.js
@@ -70,7 +70,8 @@ module.exports = function( grunt ) {
 			},
 			kss: {
 				files: [
-					'src/**/*.hbs'
+					'src/**/*.hbs',
+					'src/**/**/*.hbs'
 				],
 				tasks: [ 'styles' ],
 				options: {
