b0573b920723d296ca06d7baf87933f73164a65a
philcable <pbcable@bu.edu>
2019-07-31 15:24:46 -0700

    Register the Click to Tweet block

--- src/blocks/clicktotweet/clicktotweet.js
+++ src/blocks/clicktotweet/clicktotweet.js
@@ -1,10 +1,106 @@
 /**
- * BLOCK: bu-sample-cgb
+ * BLOCK: bu-editorial-click-to-tweet
  *
- * Registering a basic block with Gutenberg.
- * Simple block, renders and saves the same content without any interactivity.
+ * Registers a Click to Tweet block.
  */
 
-//  Import CSS.
+// External dependencies.
+import classnames from 'classnames';
+
+// Import internal dependencies.
+import './highlight-format';
+
+// Import CSS.
 import './style.scss';
 import './editor.scss';
+
+// WordPress dependencies.
+const {
+	__,
+} = wp.i18n;
+const {
+	registerBlockType,
+} = wp.blocks;
+const {
+	Path,
+	SVG,
+} = wp.components;
+const {
+	RichText,
+} = wp.editor;
+
+const getClasses = ( content ) => {
+	return (
+		classnames(
+			'wp-block-clicktotweet',
+			{
+				'has-format-highlight': content.includes( '<span class="wp-block-clicktotweet-highlight">' ),
+			}
+		)
+	);
+}
+
+// Register the block.
+registerBlockType( 'bu/clicktotweet', {
+	title: __( 'Click to Tweet' ),
+	description: __( 'Add content for readers to share via Twitter.' ),
+	icon: <SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><Path fill="#c00" d="M19 7h-1V5h-4v2h-4V5H6v2H5c-1.1 0-2 .9-2 2v10h18V9c0-1.1-.9-2-2-2zm0 10H5V9h14v8z"></Path></SVG>,
+	category: 'bu',
+	attributes: {
+		content: {
+			type: 'string',
+			default: '',
+			source: 'html',
+			selector: '.wp-block-clicktotweet-content',
+		},
+	},
+	supports: {
+		className: false,
+		customClassName: false,
+	},
+
+	edit( props ) {
+		const {
+			attributes: {
+				content,
+			},
+			setAttributes,
+		} = props;
+
+		return (
+			<p className={ getClasses( content ) }>
+				<RichText
+					className="wp-block-clicktotweet-content"
+					tagName="span"
+					formattingControls={ [ 'highlight', 'bold', 'italic' ] }
+					onChange={ value => setAttributes( { content: value } ) }
+					placeholder={ __( 'Start writing…' ) }
+					value={ content }
+					keepPlaceholderOnFocus
+				/>
+				{ !content.includes( '<span class="wp-block-clicktotweet-highlight">' ) &&
+					<button className="wp-block-clicktotweet-action js-wp-block-clicktotweet-action">Tweet this</button>
+				}
+			</p>
+		);
+	},
+
+	save( { attributes } ) {
+		const {
+			content,
+		} = attributes;
+
+		return (
+			<p className={ getClasses( content ) }>
+				<RichText.Content
+					className="wp-block-clicktotweet-content"
+					tagName="span"
+					value={ content }
+				/>
+				{ !content.includes( '<span class="wp-block-clicktotweet-highlight">' ) &&
+					<button className="wp-block-clicktotweet-action js-wp-block-clicktotweet-action">Tweet this</button>
+				}
+			</p>
+		);
+	}
+} );
---
+++ src/blocks/clicktotweet/highlight-format.js
@@ -0,0 +1,53 @@
+/**
+ * Format: bu-clicktotweet-highlight
+ *
+ * A RichText format button for wrapping selected text in a span with the
+ * 'wp-block-editorial-headline-posttext' span.
+ */
+
+// Import WordPress dependencies
+const {
+	__,
+} = wp.i18n;
+const {
+	Fragment,
+} = wp.element;
+const {
+	registerFormatType,
+	toggleFormat,
+} = wp.richText;
+const {
+	RichTextToolbarButton,
+	RichTextShortcut,
+} = wp.editor;
+
+const name = 'bu/clicktotweet-highlight';
+
+registerFormatType( name, {
+	title: __( 'Highlight' ),
+	tagName: 'span',
+	className: 'wp-block-clicktotweet-highlight',
+
+	edit( { isActive, value, onChange } ) {
+		const onToggle = () => onChange( toggleFormat( value, { type: name } ) );
+
+		return (
+			<Fragment>
+				<RichTextShortcut
+					type="access"
+					character="b"
+					onUse={ onToggle }
+				/>
+				<RichTextToolbarButton
+					name="highlight"
+					icon="twitter"
+					title={ __( 'Highlight' ) }
+					onClick={ onToggle }
+					isActive={ isActive }
+					shortcutType="access"
+					shortcutCharacter="b"
+				/>
+			</Fragment>
+		);
+	},
+} );
