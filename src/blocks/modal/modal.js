9271af1864d8a0536a754f98044622419ff62f75
Jeremy Felt <jeremy.felt@gmail.com>
2019-08-01 16:01:32 -0700

    Conditionally use editor / block-editor selectors and actions
    Some selectors and actions were moved to `core/block-editor` in
    WordPres 5.2

--- src/blocks/modal/modal.js
+++ src/blocks/modal/modal.js
@@ -22,8 +22,16 @@ const {
 	getColorClassName,
 	InnerBlocks,
 } = ( 'undefined' === typeof wp.blockEditor ) ? wp.editor : wp.blockEditor;
-const { select } = wp.data;
-const { hasSelectedInnerBlock, isBlockSelected } = select( 'core/editor' );
+const {
+	select,
+} = wp.data;
+
+// Populate selectors that were in core/editor until WordPress 5.2 and are
+// now located in core/block-editor.
+const {
+	hasSelectedInnerBlock,
+	isBlockSelected
+} = ( 'undefined' === typeof select( 'core/block-editor' ) ) ? select( 'core/editor' ) : select( 'core/block-editor' );
 
 // Register the block.
 registerBlockType( 'editorial/modal', {
