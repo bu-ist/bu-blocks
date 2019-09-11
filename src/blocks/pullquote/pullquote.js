3d9af5afad4c02ac5f44308f9c3ad9e03a3e0b73
acketon <acketon@gmail.com>
2019-09-11 14:45:44 -0400

    Rename block in Block Inserter to distinguish from default pullquote block

--- src/blocks/pullquote/pullquote.js
+++ src/blocks/pullquote/pullquote.js
@@ -73,7 +73,7 @@ const allowedMedia = [ 'image' ];
 
 // Register the block.
 registerBlockType( 'bu/pullquote', {
-	title: __( 'Pullquote' ),
+	title: __( 'BU Pullquote' ),
 	description: __( '' ),
 	icon: <SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><Path fill="#c00" d="M19 7h-1V5h-4v2h-4V5H6v2H5c-1.1 0-2 .9-2 2v10h18V9c0-1.1-.9-2-2-2zm0 10H5V9h14v8z"></Path></SVG>,
 	category: 'bu',
