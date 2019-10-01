efa403bcdc35f65d20f066cdc036ebd85f2723fa
acketon <acketon@gmail.com>
2019-10-01 16:16:23 -0400

    increment and build

--- bu-blocks.php
+++ bu-blocks.php
@@ -7,7 +7,7 @@
  * Author URI:      http://www.bu.edu/interactive-design/
  * Text Domain:     bu-blocks
  * Domain Path:     /languages
- * Version:         0.2.13
+ * Version:         0.2.14
  *
  * @package         BU_Blocks
  */
@@ -20,7 +20,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Defines the plugin version.
-define( 'BU_BLOCKS_VERSION', '0.2.13' );
+define( 'BU_BLOCKS_VERSION', '0.2.14' );
 
 /**
  * Displays admin notice and prevents activation.
