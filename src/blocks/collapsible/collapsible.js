cbf2c2799fe4b789e1ac720d8ba58d25570fd9a5
Kyle Evans <kyle@kyleevans.io>
2020-08-04 11:20:44 +0200

    Add anchor support

--- src/blocks/collapsible/collapsible.js
+++ src/blocks/collapsible/collapsible.js
@@ -36,6 +36,9 @@ registerBlockType( 'editorial/collapsible', {
 	description: __( 'A collapsible content block.' ),
 	icon: <SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><Path fill="#c00" d="M19 7h-1V5h-4v2h-4V5H6v2H5c-1.1 0-2 .9-2 2v10h18V9c0-1.1-.9-2-2-2zm0 10H5V9h14v8z"></Path></SVG>,
 	category: 'bu-editorial',
+	supports: {
+		anchor: true
+	},
 	attributes: {
 		content: {
 			type: 'string',
