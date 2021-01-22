e442f69a184c41ef1855cb6ed9a55d7edacff013
david <david.chandra@10up.com>
2021-01-22 13:43:17 +0700

    Add wide and full-width support.

--- src/blocks/collapsible/collapsible.js
+++ src/blocks/collapsible/collapsible.js
@@ -46,7 +46,9 @@ registerBlockType( 'bu/collapsible', {
 	description: __( 'A collapsible content block.' ),
 	icon: <SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><Path fill="#c00" d="M19 7h-1V5h-4v2h-4V5H6v2H5c-1.1 0-2 .9-2 2v10h18V9c0-1.1-.9-2-2-2zm0 10H5V9h14v8z"></Path></SVG>,
 	category: 'bu',
-	supports: {},
+	supports: {
+		align: [ 'wide', 'full' ],
+	},
 	attributes: {
 		title: {
 			type: 'string',
