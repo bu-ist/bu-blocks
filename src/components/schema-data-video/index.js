264cab74f66b1fb53932f57916ef972476b3fe2a
acketon <acketon@gmail.com>
2021-03-26 09:58:48 -0400

    refactor and cleanup attributes and json data structure

--- src/components/schema-data-video/index.js
+++ src/components/schema-data-video/index.js
@@ -13,12 +13,17 @@ const {
 } = wp.i18n;
 const {
 	Fragment,
+	useState,
 } = wp.element;
 const {
 	PanelBody,
 	ToggleControl,
 	TextControl,
 	TextareaControl,
+	DateTimePicker,
+	Popover,
+	Button,
+	BaseControl,
 } = wp.components;
 const {
 	InspectorControls,
@@ -26,7 +31,7 @@ const {
 
 // Schema Data attributes.
 const SchemaDataVideoAttributes = {
-	schemaDataVideoDisabled: {
+	schemaDataVideoShow: {
 		type: 'boolean',
 		default: false,
 	},
@@ -37,16 +42,18 @@ const SchemaDataVideoAttributes = {
 	videoDescription: {
 		type: 'string',
 		default: '',
-		source: 'text',
-		selector: '.wp-blocks-components-schema-data-video-tools'
+	},
+	videouploadDate: {
+		type: 'string',
+		default: '',
 	},
 	schemaJSON: {
 		type: 'object',
 		default: {
 			"@context": "http://schema.org",
 			"@type": "VideoObject",
-			"name": "test",
-			"description": "",
+			"name": '',
+			"description": '',
 			"thumbnailUrl": [],
 			"uploadDate": "",
 			"duration": "",
@@ -56,10 +63,22 @@ const SchemaDataVideoAttributes = {
 			"productionCompany": "",
 			"keywords": ""
 		},
-		source: 'text',
-		selector: 'script'
-	}
+	},
 };
+
+
+const SchemaDataVideoJSON = function( props ) {
+
+	const { json } = props;
+
+
+	return(
+		<script className="wp-blocks-components-schema-data-video-tools" type="application/ld+json">
+			{ JSON.stringify( json ) }
+		</script>
+
+	)
+}
 
 /**
  * The Schema Data Video component.
@@ -67,6 +86,7 @@ const SchemaDataVideoAttributes = {
  * @param {array} props The properties passed to the component.
  */
 function SchemaDataVideoTools( props ) {
+
 	// Get the properties of this component.
 	const {
 		blockProps,
@@ -80,73 +100,101 @@ function SchemaDataVideoTools( props ) {
 
 	// Get the attributes for handling the background data.
 	const {
-		schemaDataVideoDisabled,
-		videoName,
-		videoDescription,
+		schemaDataVideoShow,
 		schemaJSON,
 	} = attributes;
 
+	// Setup states for date picker for uploadDate value.
+	const [
+		openDatePopup,
+		setOpenDatePopup
+	] = useState( false );
+
 	const updateSchema = function( key, value ) {
-		console.log( schemaJSON[key] );
+		console.log( value );
 		var newSchema = JSON.parse( JSON.stringify( schemaJSON ) );
+
 		newSchema[key] = value;
-		console.log( newSchema[key] )
 
-		setAttributes( { schemaJSON: newSchema } );
+		setAttributes( { schemaJSON: newSchema  } );
 	}
-
-	// function get_schema( videoName, videoDescription, thumbnails, uploadDate, duration, contentUrl, embedUrl, director, productionCompany, keywords ) {
-	// 	return {
-	// 		"@context": "http://schema.org",
-	// 		"@type": "VideoObject",
-	// 		"name": "Drone Footage Captures Deserted Boston University Campus During Coronavirus Pandemic",
-	// 		"description": "On April 23, 2020, when Boston University’s Campus would normally have been buzzing with activity, it was an alternate universe. This drone footage, shot that day, shows an eerily quiet landscape, vividly capturing the impact of the coronavirus pandemic on the BU campus.",
-	// 		"thumbnailUrl": [
-	// 				"https://www.bu.edu/files/2020/05/20-1225-DRONE-024-1-1.jpg",
-	// 				"https://www.bu.edu/files/2020/05/4x3-20-1225-DRONE-024.jpg",
-	// 				"https://www.bu.edu/files/2020/05/16x9-20-1225-DRONE-024.jpg"
-	// 				],
-	// 		"uploadDate": "2020-05-12T12:30:00",
-	// 		"duration": "PT2M45S",
-	// 		"contentUrl": "https://www.youtube.com/watch?v=6kRtbn_bNNU",
-	// 		"embedUrl": "https://www.bu.edu/buniverse/interface/embed/embed.html?v=2EFp6m2Eo",
-	// 		"director": "Above Summit and Chris Palmer",
-	// 		"productionCompany": "Boston University",
-	// 		"keywords": "drone footage, drone video, drone videos, 4k drone footage, 4k video, 4k videos, aerial video, aerial videos, aerial footage, boston university campus, bu campus, covid-19, covid-19 pandemic, coronavirus,coronavirus pandemic, boston drone videos, higher education, higher education videos, college campus, college campuses, college campus videos, boston university, charles river campus, above summit, bu productions"
-	// 	}
-	// }
 
 	// Return the interface for the background component.
 	return (
 		<Fragment>
 			<InspectorControls>
-				<PanelBody title={ __( 'Schema Data Options' ) }>
+				<PanelBody title={ __( 'Video Schema Data' ) }>
 					<ToggleControl
-						label={ __( 'Disable Schema Data' ) }
-						checked={ schemaDataVideoDisabled }
-						onChange={ () => setAttributes( { schemaDataVideoDisabled: !schemaDataVideoDisabled } ) }
+						label={ __( 'Include Video Schema' ) }
+						checked={ schemaDataVideoShow }
+						onChange={ () => setAttributes( { schemaDataVideoShow: !schemaDataVideoShow } )	}
 					/>
-					<TextControl
-						label={ __( 'Video Name' ) }
-						value={ schemaJSON.name }
-						onChange={ ( value) => updateSchema( 'name', value ) }
-						//onChange={ ( value ) => setAttributes( { schemaJSON: { ...schemaJSON, [name]: value } } ) }
-					/>
-					<TextareaControl
-						label={ __( 'Description' ) }
-						value={ videoDescription }
-						help={ __( 'Enter a description for the video') }
-						onChange={ ( value ) => setAttributes( { videoDescription: value } ) }
-					/>
+					{ schemaDataVideoShow && (
+						<Fragment>
+							<TextControl
+								label={ __( 'Video Name' ) }
+								value={ schemaJSON.name }
+								onChange={ ( value ) => updateSchema( 'name', value ) }
+							/>
+
+							<TextareaControl
+								label={ __( 'Description' ) }
+								value={ schemaJSON.description }
+								help={ __( 'Enter a description for the video') }
+								onChange={ ( value ) => updateSchema( 'description', value ) }
+							/>
+							<Fragment>
+								<BaseControl
+									help="Enter the Upload Date of the video"
+								>
+									<BaseControl.VisualLabel>
+										Upload Date
+									</BaseControl.VisualLabel>
+									<Button
+										isLink={true}
+										onClick={() => setOpenDatePopup( ! openDatePopup )}
+									>
+										{ schemaJSON.uploadDate ? schemaJSON.uploadDate : 'Set Date' }
+
+
+										{ openDatePopup && (
+											<Popover
+												position="bottom right"
+												onClickOutside={ () => setOpenDatePopup( ! openDatePopup ) }
+												noArrow={ false }
+												onClose={ setOpenDatePopup.bind( null, false )}
+											>
+												<DateTimePicker
+													currentDate={ schemaJSON.uploadDate ? schemaJSON.uploadDate : new Date().toLocaleString() }
+													onChange={ ( value ) => updateSchema( 'uploadDate', value ) }
+													is12Hour={ true }
+												/>
+										</Popover>
+										) }
+									</Button>
+									{ schemaJSON.uploadDate && (
+										<Button
+											isLink={true}
+											onClick={() => updateSchema( 'uploadDate', '' ) }
+										>Clear</Button>
+									)}
+								</BaseControl>
+							</Fragment>
+
+							<TextControl
+								label={ __( 'Duration' ) }
+								value={ schemaJSON.duration }
+								onChange={ ( value ) => updateSchema( 'duration', value ) }
+							/>
+						</Fragment>
+					) }
 				</PanelBody>
 			</InspectorControls>
-			{ console.log( schemaJSON ) }
-			{ ! schemaDataVideoDisabled && (
-				<script className="wp-blocks-components-schema-data-video-tools" type="application/ld+json">
-					{/* <a href="#" className="icon-action">{ __( 'Share this' ) }</a> */}
-					{ JSON.stringify( schemaJSON ) }
 
-				</script>
+			{ schemaDataVideoShow && (
+				<SchemaDataVideoJSON
+					json={ schemaJSON }
+				/>
 			) }
 		</Fragment>
 	);
@@ -155,6 +203,7 @@ function SchemaDataVideoTools( props ) {
 // Export attributes for easy importing in blocks.
 export {
 	SchemaDataVideoAttributes,
+	SchemaDataVideoJSON,
 };
 
 // Export the Schema Data Video Tools control panel.
