802c0b3db61abf97ce9985aebad07a3b46c347cd
Sk Saju <sksaaju@gmail.com>
2023-03-14 00:44:41 +0600

    fixed stats block issue on wp 5.7

--- src/blocks/stat/stats.js
+++ src/blocks/stat/stats.js
@@ -13,51 +13,29 @@ import './editor.scss';
 
 // Internal dependencies.
 import './stat';
-import getAllowedFormats from '../../global/allowed-formats';
 import blockIcons from '../../components/block-icons/';
+import edit from './edit';
 
 // WordPress dependencies.
 const {
 	__,
 } = wp.i18n;
 const {
-	createBlock,
 	registerBlockType,
 } = wp.blocks;
-const {
-	PanelBody,
-	Path,
-	RangeControl,
-	SVG,
-} = wp.components;
+
 const {
 	InnerBlocks,
-	InspectorControls,
 	RichText,
 } = ( 'undefined' === typeof wp.blockEditor ) ? wp.editor : wp.blockEditor;
-const {
-	dispatch,
-	select,
-} = wp.data;
-
-// Populate selectors that were in core/editor until WordPress 5.2 and are
-// now located in core/block-editor.
-const {
-	getBlocksByClientId,
-} = ( 'undefined' === typeof select( 'core/block-editor' ) ) ? select( 'core/editor' ) : select( 'core/block-editor' );
-
-// Populate actions that were in core/editor until WordPress 5.2 and are
-// now located in core/block-editor.
-const {
-	removeBlock,
-	insertBlock,
-} = ( 'undefined' === typeof dispatch( 'core/block-editor' ) ) ? dispatch( 'core/editor' ) : dispatch( 'core/block-editor' );
 
 /**
  * Returns the class list for the block based on the current settings.
  *
  * @param {string} className Default classes assigned to the block.
  * @param {number} stats     The number of stat blocks to display.
+ *
+ * @return {string} block classnames
  */
 const getBlockClasses = ( className, stats ) => {
 	return (
@@ -93,98 +71,14 @@ registerBlockType( 'bu/stats', {
 		},
 		stats: {
 			type: 'number',
-			default: '',
+			default: 1,
 		},
 	},
 	supports: {
 		align: [ 'left', 'right', 'wide' ],
 	},
 
-	edit( props ) {
-		const {
-			attributes: {
-				align,
-				caption,
-				stats,
-			},
-			className,
-			clientId,
-			setAttributes,
-		} = props;
-
-		// Determine whether the block is aligned left or right.
-		const isAlignedLeftOrRight = ( align === 'left' || align === 'right' );
-
-		/**
-		 * Updates the stats attribute and handles innerBlock insertions or removals.
-		 *
-		 * @param {string} statNumber Current value of the stat number option.
-		 */
-		const onChangeStatsNumber = ( statNumber ) => {
-			setAttributes( { stats: statNumber } );
-
-			// Get the current innerBlocks.
-			const blocks = getBlocksByClientId( clientId )[ 0 ].innerBlocks;
-
-			// Remove the last innerBlock if there are more innerBlocks than columns.
-			if ( blocks.length > statNumber && blocks[ statNumber ] ) {
-				removeBlock( blocks[ statNumber ].clientId, false );
-			} else {
-				// Otherwise, create and insert a new block.
-				const newBlock = createBlock( 'bu/stat', {} );
-				const index = statNumber - 1;
-
-				insertBlock( newBlock, index, clientId );
-			}
-		};
-
-		// Set a child Stat block when first inserted.
-		if ( stats === '' ) {
-			onChangeStatsNumber( 1 );
-		}
-
-		// Reset to a single Stat block if aligned left or right.
-		if ( isAlignedLeftOrRight && stats !== 1 ) {
-			onChangeStatsNumber( 1 );
-		}
-
-		return (
-			<div className={ getBlockClasses( className, stats ) }>
-				<figure className="wp-block-bu-stats-figure">
-					<div className="wp-block-bu-stats-row">
-						<InnerBlocks
-							allowedBlocks={ [ 'bu/stat' ] }
-							templateLock={ false }
-						/>
-						{ !isAlignedLeftOrRight &&
-							<InspectorControls>
-								<PanelBody title={ __( 'Options' ) }>
-									<RangeControl
-										label={ __( 'Number of Stats' ) }
-										value={ stats }
-										onChange={ onChangeStatsNumber }
-										min={ 1 }
-										max={ 4 }
-										step={ 1 }
-									/>
-								</PanelBody>
-							</InspectorControls>
-						}
-					</div>
-					<RichText
-						tagName="figcaption"
-						className="wp-block-bu-stats-caption"
-						placeholder={ __( 'Add a caption (optional)…' ) }
-						value={ caption }
-						onChange={ value => setAttributes( { caption: value } ) }
-						formattingControls={ getAllowedFormats( 'formattingControls', [ 'bold', 'italic', 'link' ] ) }
-						allowedFormats={ getAllowedFormats( 'allowedFormats', [ 'core/bold', 'core/italic', 'core/link' ] ) }
-						keepPlaceholderOnFocus
-					/>
-				</figure>
-			</div>
-		);
-	},
+	edit,
 
 	save( { attributes } ) {
 		const {
