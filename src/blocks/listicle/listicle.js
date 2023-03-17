cb09d709b608f85d770310275dbef5ef25db3e62
Sk Saju <sksaaju@gmail.com>
2023-03-17 23:04:40 +0600

    refacor edict component for listcle block

--- src/blocks/listicle/listicle.js
+++ src/blocks/listicle/listicle.js
@@ -25,12 +25,7 @@ const {
 	registerBlockType,
 } = wp.blocks;
 const {
-	Component,
-} = wp.element;
-const {
 	PanelBody,
-	Path,
-	SVG,
 	ToggleControl,
 } = wp.components;
 const {
@@ -38,6 +33,7 @@ const {
 	RichText,
 	PlainText,
 } = ( 'undefined' === typeof wp.blockEditor ) ? wp.editor : wp.blockEditor;
+const { useEffect } = wp.element;
 
 /**
  * Returns the class list for the block based on the current settings.
@@ -130,192 +126,173 @@ registerBlockType( 'editorial/listicle', {
 		...ShareToolsAttributes,
 	},
 
-	edit: class extends Component {
-		constructor() {
-			super( ...arguments );
-		}
-
+	edit( props ) {
+		// Get the block properties.
+		const {
+			attributes,
+			setAttributes,
+			className,
+			isSelected,
+		} = props;
+	
+		// Get the block attributes.
+		const {
+			hed,
+			dek,
+			content,
+			aside,
+			number,
+			related,
+			credit,
+			backgroundUrl,
+			backgroundAutoplay,
+			backgroundCaption,
+			divider,
+		} = attributes;
+	
 		/**
 		 * Update credit attribute with the caption of the selected image.
 		 *
 		 * @param {object} prevProps The property values before the change.
 		 */
-		componentDidUpdate( prevProps ) {
-			// Get the block properties we need.
-			const {
-				attributes,
-				setAttributes,
-			} = this.props;
-
-			// Get the block attributes we need.
-			const {
-				credit,
-				backgroundCaption,
-			} = attributes;
-
+		useEffect( () => {
 			// Stop here if the `backgroundCaption` attribute hasn't changed.
-			if ( backgroundCaption === prevProps.attributes.backgroundCaption ) {
+			if ( backgroundCaption === backgroundCaption ) {
 				return;
 			}
-
+	
 			// Stop here if the `credit` attribute is already set.
 			if ( !! credit || ! backgroundCaption ) {
 				return;
 			}
-
+	
 			// Update the `credit` attribute using the caption from the selected image.
 			setAttributes( { credit: backgroundCaption } );
-		};
-
-		render() {
-			// Get the block properties.
-			const {
-				attributes,
-				setAttributes,
-				className,
-				isSelected,
-			} = this.props;
-
-			// Get the block attributes.
-			const {
-				hed,
-				dek,
-				content,
-				aside,
-				number,
-				related,
-				credit,
-				backgroundUrl,
-				backgroundAutoplay,
-				divider,
-			} = attributes;
-
-			// Check if the block has aside content (extra condition due to use of multiline).
-			const hasAsideContent = ( ! RichText.isEmpty( aside ) && aside !== '<br>' );
-
-			/**
-			 * Get a value to use for the inline width of the number input.
-			 *
-			 * Returns either 100% if the field is empty, or `{n}ch`,
-			 * where `{n}` is the number of characters in the input.
-			 *
-			 */
-			const getNumberInputWidth = () => {
-				return ( number ) ? number.length + 'ch' : '100%';
-			};
-
-			// Return the block editing interface.
-			return (
-				<section className={ getClasses( className, number, hasAsideContent, backgroundUrl, backgroundAutoplay, divider ) }>
-					<article className="wp-block-editorial-listicle-article">
-						<figure className="wp-block-editorial-listicle-figure">
-							<Background
-								blockProps={ this.props }
-								inlinePlaceholder={ true }
-								options={ [] }
-							/>
-							<RichText
-								tagName="figcaption"
-								className="wp-caption-text wp-block-editorial-listicle-caption wp-prepress-component-caption"
-								value={ credit }
-								onChange={ value => setAttributes( { credit: value } ) }
-								placeholder={ __( 'Add Photo or Video Credit…' ) }
-								formattingControls={ getAllowedFormats( 'formattingControls', [ 'bold', 'italic', 'link' ] ) }
-								allowedFormats={ getAllowedFormats( 'allowedFormats', [ 'core/bold', 'core/italic', 'core/link' ] ) }
-								keepPlaceholderOnFocus
-							/>
-						</figure>
-						<header className="wp-block-editorial-listicle-header">
-							{ ( number || isSelected ) && (
-								<h2 className="wp-block-editorial-listicle-header-number">
-									<PlainText
-										placeholder={ __( 'Add Item Number (Optional)…' ) }
-										value={ number }
-										onChange={ number => setAttributes( { number } ) }
-										style={ {
-											width: getNumberInputWidth(),
-										} }
-									/>
-								</h2>
-							) }
-							<div className="wp-block-editorial-listicle-header-content">
-								<RichText
-									tagName="h3"
-									className="wp-block-editorial-listicle-header-content-hed"
-									placeholder={ __( 'Add Title…' ) }
-									value={ hed }
-									onChange={ value => setAttributes( { hed: value } ) }
-									formattingControls={ getAllowedFormats( 'formattingControls', [ 'bold', 'italic' ] ) }
-									allowedFormats={ getAllowedFormats( 'allowedFormats', [ 'core/bold', 'core/italic' ] ) }
+		}, [] );
+	
+		// Check if the block has aside content (extra condition due to use of multiline).
+		const hasAsideContent = ! RichText.isEmpty( aside ) && aside !== '<br>';
+	
+		/**
+		 * Get a value to use for the inline width of the number input.
+		 *
+		 * Returns either 100% if the field is empty, or `{n}ch`,
+		 * where `{n}` is the number of characters in the input.
+		 *
+		 */
+		const getNumberInputWidth = ( number ) ? number.length + 'ch' : '100%';
+	
+		// Return the block editing interface.
+		return (
+			<section className={ getClasses( className, number, hasAsideContent, backgroundUrl, backgroundAutoplay, divider ) }>
+				<article className="wp-block-editorial-listicle-article">
+					<figure className="wp-block-editorial-listicle-figure">
+						<Background
+							blockProps={ props }
+							inlinePlaceholder={ true }
+							options={ [] }
+						/>
+						<RichText
+							tagName="figcaption"
+							className="wp-caption-text wp-block-editorial-listicle-caption wp-prepress-component-caption"
+							value={ credit }
+							onChange={ value => setAttributes( { credit: value } ) }
+							placeholder={ __( 'Add Photo or Video Credit…' ) }
+							formattingControls={ getAllowedFormats( 'formattingControls', [ 'bold', 'italic', 'link' ] ) }
+							allowedFormats={ getAllowedFormats( 'allowedFormats', [ 'core/bold', 'core/italic', 'core/link' ] ) }
+							keepPlaceholderOnFocus
+						/>
+					</figure>
+					<header className="wp-block-editorial-listicle-header">
+						{ ( number || isSelected ) && (
+							<h2 className="wp-block-editorial-listicle-header-number">
+								<PlainText
+									placeholder={ __( 'Add Item Number (Optional)…' ) }
+									value={ number }
+									onChange={ number => setAttributes( { number } ) }
+									style={ {
+										width: getNumberInputWidth,
+									} }
 								/>
-								<RichText
-									tagName="h4"
-									className="wp-block-editorial-listicle-header-content-dek"
-									placeholder={ __( 'Add Subtitle…' ) }
-									value={ dek }
-									onChange={ value => setAttributes( { dek: value } ) }
-									formattingControls={ getAllowedFormats( 'formattingControls', [ 'bold', 'italic' ] ) }
-									allowedFormats={ getAllowedFormats( 'allowedFormats', [ 'core/bold', 'core/italic' ] ) }
-								/>
-							</div>
-						</header>
-						<section className="wp-block-editorial-listicle-section">
-							<RichText
-								tagName="div"
-								className="wp-block-editorial-listicle-section-content"
-								multiline="p"
-								placeholder={ __( 'Add Content… lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in dictum felis. Nullam gravida dui nunc, vitae tristique ex pellentesque at. Suspendisse id porttitor metus. Nullam et ipsum hendrerit urna mattis porttitor at in leo.' ) }
-								value={ content }
-								onChange={ value => setAttributes( { content: value } ) }
-								formattingControls={ getAllowedFormats( 'formattingControls', [ 'bold', 'italic', 'link' ] ) }
-								allowedFormats={ getAllowedFormats( 'allowedFormats', [ 'core/bold', 'core/italic', 'core/link' ] ) }
-							/>
-							<div className="wp-block-editorial-listicle-section-meta">
-								{ ( hasAsideContent || isSelected ) && (
-									<aside className="wp-block-editorial-listicle-section-aside">
-										<RichText
-											tagName="p"
-											placeholder={ __( 'Add Sidebar (Optional)…' ) }
-											value={ aside }
-											onChange={ value => setAttributes( { aside: value } ) }
-											formattingControls={ getAllowedFormats( 'formattingControls', [ 'bold', 'italic', 'link' ] ) }
-											allowedFormats={ getAllowedFormats( 'allowedFormats', [ 'core/bold', 'core/italic', 'core/link' ] ) }
-										/>
-									</aside>
-								) }
-								<ShareTools
-									blockProps={ this.props }
-								/>
-							</div>
-						</section>
-						{ ( hasRelatedLinks( related ) || isSelected ) && (
-							<footer className="wp-block-editorial-listicle-footer">
-								<h3 className="wp-block-editorial-listicle-footer-title">Related Stories</h3>
-								<RichText
-									tagName="ul"
-									multiline="li"
-									className="wp-block-editorial-listicle-footer-list"
-									placeholder={ __( 'Enter Related Stories List…' ) }
-									value={ related }
-									onChange={ ( value ) => setAttributes( { related: value } ) }
-									formattingControls={ getAllowedFormats( 'formattingControls', [ 'link' ] ) }
-									allowedFormats={ getAllowedFormats( 'allowedFormats', [ 'core/link' ] ) }
-								/>
-							</footer>
+							</h2>
 						) }
-					</article>
-					<InspectorControls>
-						<PanelBody title={ __( 'Display Options' ) }>
-							<ToggleControl
-								label={ __( 'Show Bottom Divider' ) }
-								checked={ divider }
-								onChange={ () => setAttributes( { divider: !divider } ) }
+						<div className="wp-block-editorial-listicle-header-content">
+							<RichText
+								tagName="h3"
+								className="wp-block-editorial-listicle-header-content-hed"
+								placeholder={ __( 'Add Title…' ) }
+								value={ hed }
+								onChange={ value => setAttributes( { hed: value } ) }
+								formattingControls={ getAllowedFormats( 'formattingControls', [ 'bold', 'italic' ] ) }
+								allowedFormats={ getAllowedFormats( 'allowedFormats', [ 'core/bold', 'core/italic' ] ) }
 							/>
-						</PanelBody>
-					</InspectorControls>
-				</section>
-			);
-		};
+							<RichText
+								tagName="h4"
+								className="wp-block-editorial-listicle-header-content-dek"
+								placeholder={ __( 'Add Subtitle…' ) }
+								value={ dek }
+								onChange={ value => setAttributes( { dek: value } ) }
+								formattingControls={ getAllowedFormats( 'formattingControls', [ 'bold', 'italic' ] ) }
+								allowedFormats={ getAllowedFormats( 'allowedFormats', [ 'core/bold', 'core/italic' ] ) }
+							/>
+						</div>
+					</header>
+					<section className="wp-block-editorial-listicle-section">
+						<RichText
+							tagName="div"
+							className="wp-block-editorial-listicle-section-content"
+							multiline="p"
+							placeholder={ __( 'Add Content… lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in dictum felis. Nullam gravida dui nunc, vitae tristique ex pellentesque at. Suspendisse id porttitor metus. Nullam et ipsum hendrerit urna mattis porttitor at in leo.' ) }
+							value={ content }
+							onChange={ value => setAttributes( { content: value } ) }
+							formattingControls={ getAllowedFormats( 'formattingControls', [ 'bold', 'italic', 'link' ] ) }
+							allowedFormats={ getAllowedFormats( 'allowedFormats', [ 'core/bold', 'core/italic', 'core/link' ] ) }
+						/>
+						<div className="wp-block-editorial-listicle-section-meta">
+							{ ( hasAsideContent || isSelected ) && (
+								<aside className="wp-block-editorial-listicle-section-aside">
+									<RichText
+										tagName="p"
+										placeholder={ __( 'Add Sidebar (Optional)…' ) }
+										value={ aside }
+										onChange={ value => setAttributes( { aside: value } ) }
+										formattingControls={ getAllowedFormats( 'formattingControls', [ 'bold', 'italic', 'link' ] ) }
+										allowedFormats={ getAllowedFormats( 'allowedFormats', [ 'core/bold', 'core/italic', 'core/link' ] ) }
+									/>
+								</aside>
+							) }
+							<ShareTools
+								blockProps={ props }
+							/>
+						</div>
+					</section>
+					{ ( hasRelatedLinks( related ) || isSelected ) && (
+						<footer className="wp-block-editorial-listicle-footer">
+							<h3 className="wp-block-editorial-listicle-footer-title">Related Stories</h3>
+							<RichText
+								tagName="ul"
+								multiline="li"
+								className="wp-block-editorial-listicle-footer-list"
+								placeholder={ __( 'Enter Related Stories List…' ) }
+								value={ related }
+								onChange={ ( value ) => setAttributes( { related: value } ) }
+								formattingControls={ getAllowedFormats( 'formattingControls', [ 'link' ] ) }
+								allowedFormats={ getAllowedFormats( 'allowedFormats', [ 'core/link' ] ) }
+							/>
+						</footer>
+					) }
+				</article>
+				<InspectorControls>
+					<PanelBody title={ __( 'Display Options' ) }>
+						<ToggleControl
+							label={ __( 'Show Bottom Divider' ) }
+							checked={ divider }
+							onChange={ () => setAttributes( { divider: !divider } ) }
+						/>
+					</PanelBody>
+				</InspectorControls>
+			</section>
+		);
 	},
 
 	save( props ) {
