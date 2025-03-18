3015b05d252c1e5fdb031706406f204a1dd1767b
acketon <acketon@gmail.com>
2025-03-18 10:11:41 -0400

    remove object-fit-images polyfill

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '46f9ca6ea1369c9adaaa');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '60e93186d3593be419f8');
