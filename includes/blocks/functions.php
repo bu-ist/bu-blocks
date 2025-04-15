5e97069d236e818d533bc6c0f1673f43031cb960
acketon <acketon@gmail.com>
2025-04-15 16:51:12 -0400

    add block-supports import and new function that returns list of registered block names

--- includes/blocks/functions.php
+++ includes/blocks/functions.php
@@ -14,6 +14,22 @@ define( 'BU_BLOCKS_BLOCKS_BUILD_DIR', BU_BLOCKS_DIR . 'dist' );
 define( 'BU_BLOCKS_BLOCKS_BUILD_URL', BU_BLOCKS_URL . 'dist' );
 
 /**
+ * Array of Block Names
+ *
+ * @return Array of block names in this plugin.
+ * 
+ * @since 2.0.0
+ */
+function bu_blocks_get_registered_block_names() {
+	$blocks = array(
+		'editorial/aside',
+		'bu/buniverse',
+		'bu/button'
+	);
+
+	return apply_filters( 'bu_blocks_registered_block_names', $blocks );
+}
+/**
  * Block Stylesheet Handling
  *
  * Set to true to bundle all block styles together and enqueue them
@@ -39,11 +55,14 @@ function bu_blocks_decorative_block_styles() {
 /**
  * Include Blocks & Assets
  */
+require_once 'class-block-supports.php';
 require_once 'class-blocks.php';
 require_once 'class-blockenqueues.php';
 
 // Load Gutenberg Patterns.
 //require 'class-blockpatterns.php';
 
-
+/**
+ * Include Deprecated functions for old versions of blocks.
+ */
 require_once BU_BLOCKS_DIR . 'src/blocks/button/deprecated/deprecated.php';
