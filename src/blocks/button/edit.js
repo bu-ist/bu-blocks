a76ad841aa6345e0390c937fc9496a489a53f8d7
acketon <acketon@gmail.com>
2025-04-18 11:52:34 -0400

    DRY update: make PanelColorSettings into a shared component and add to button & aside blocks

--- src/blocks/button/edit.js
+++ src/blocks/button/edit.js
@@ -9,30 +9,20 @@ import classnames from 'classnames';
 
 // Internal dependencies.
 import themeOptions from '../../global/theme-options';
-import {
-	getColorSlug,
-	getColorThemesSupportsByBlock,
-} from '../../global/color-utils.mjs';
+import { getColorSlug } from '../../global/color-utils.mjs';
 import getAllowedFormats from '../../global/allowed-formats';
 import publicationSlug from '../../global/publication-slug';
+import ThemeColorPanel from '../../components/colorthemes-panel/index.js';
 
 // WordPress dependencies.
 import { __ } from '@wordpress/i18n';
-import {
-	Button,
-	PanelBody,
-	RadioControl,
-	PanelRow,
-} from '@wordpress/components';
+import { Button, PanelBody, RadioControl } from '@wordpress/components';
 import {
 	InspectorControls,
-	PanelColorSettings,
 	RichText,
 	URLInput,
 	useBlockProps,
-	getColorObjectByAttributeValues,
 } from '@wordpress/block-editor';
-import { hasBlockSupport } from '@wordpress/blocks';
 
 // The current publication owner.
 const publication = publicationSlug();
@@ -64,70 +54,24 @@ export default function Edit( props ) {
 		className: getClasses( className, themeColor, icon ),
 	} );
 
-	// themOptions() returns the full color palette added to editor settings.
-	// Set the ThemeOptions() color Palette to a variable so we can clear
-	// it if disabled by filter.
-	let themeOptionsPalette = themeOptions();
-
-	// Check if block's metadata from block.json has an entry for __bublocks_colorthemes.
-	const hasThemOptionsColorThemesSupport = hasBlockSupport(
-		name,
-		'__bublocks_colorthemes'
-	);
-
-	if ( hasThemOptionsColorThemesSupport ) {
-		// Get any block specific color themes palette set
-		// in block.json `supports.__bublocks_colorthemes`
-		themeOptionsPalette = getColorThemesSupportsByBlock(
-			name,
-			themeOptionsPalette
-		);
-	}
-
-	// Create a color object from the palette for themeColor attribute
-	//  to pass into the component.
-	const themeColorObject = getColorObjectByAttributeValues(
-		themeOptionsPalette,
-		themeColor
-	);
-
-	// Check if palette is an array and has any values.
-	const hasThemeOptionsPalette =
-		Array.isArray( themeOptionsPalette ) && themeOptionsPalette.length > 0;
+	// themOptions() returns the full/global color palette added to editor settings.
+	const themeOptionsPalette = themeOptions();
 
 	return (
 		<>
 			<InspectorControls>
-				{ hasThemOptionsColorThemesSupport && (
-					<PanelColorSettings
-						title={ __( 'Color Theme' ) }
-						colorSettings={ [
-							{
-								value: themeColorObject?.color,
-								onChange: ( value ) =>
-									setAttributes( {
-										themeColor: value
-											? getColorSlug(
-													value,
-													themeOptionsPalette
-											  )
-											: undefined,
-									} ),
-								label: __( 'Theme' ),
-								disableCustomColors: true,
-								colors: themeOptionsPalette,
-							},
-						] }
-					>
-						{ ! hasThemeOptionsPalette && (
-							<PanelRow>
-								<em>
-									No Color Palette available for this block.
-								</em>
-							</PanelRow>
-						) }
-					</PanelColorSettings>
-				) }
+				<ThemeColorPanel
+					blockname={ name }
+					value={ themeColor }
+					themepalette={ themeOptionsPalette }
+					onChange={ ( value, palette ) =>
+						setAttributes( {
+							themeColor: value
+								? getColorSlug( value, palette )
+								: undefined,
+						} )
+					}
+				/>
 				<PanelBody title={ __( 'Icon Settings' ) }>
 					<RadioControl
 						label="Placement"
--- src/global/color-utils.mjs
+++ src/global/color-utils.mjs
@@ -3,7 +3,7 @@
  */
 
 import { getColorObjectByColorValue } from '@wordpress/block-editor';
-import { getBlockSupport } from '@wordpress/blocks';
+
 
 /**
  * When given a color it gets the Color Slug from the themeoptions() color
@@ -14,34 +14,16 @@ import { getBlockSupport } from '@wordpress/blocks';
  * @return {string} The slug of the color.
  */
 export const getColorSlug = ( color, palette ) => {
+	console.log("getcolorslug ", color, palette );
 	if ( color ) {
 		const colorObject = getColorObjectByColorValue( palette, color );
 
 		if ( colorObject.slug ) {
+			console.log("getcolorslug", colorObject.slug);
 			return colorObject.slug;
 		}
 	} else {
 		console.error( 'Error: no color.slug value found in color object.' ); // eslint-disable-line no-console
 	}
 	return undefined;
-};
-
-/**
- * Get ColorThemes setting from the block's Supports array in block.json
- * and override the site-wide color palette set in the theme. 
- * @param {*} name
- * @param {*} palette
- * @return {Array} A color palette array.
- */
-export const getColorThemesSupportsByBlock = ( name, palette ) => {
-	const BlockColorThemesPalette = getBlockSupport(
-		name,
-		'__bublocks_colorthemes'
-	);
-
-	if ( Array.isArray( BlockColorThemesPalette ) ) {
-		palette = BlockColorThemesPalette;
-	}
-
-	return palette;
 };
